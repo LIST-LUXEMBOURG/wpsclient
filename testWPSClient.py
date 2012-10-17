b002b1cc24b0811a59a667924d5bc20a16f3c75b
ldesousa <luis.desousa@tudor.lu>
2012-10-17 14:14:20 +0000

    Included logging mechanism.
    git-svn-id: http://crteweb.tudor.lu/svn/music/code/lu.Tudor.CRTE.MUSIC.WPSClient/src@852 5f484053-1017-4574-b885-055e9ad5cbe3

--- testWPSClient.py
+++ testWPSClient.py
@@ -25,7 +25,7 @@ iniCli = WPSClient.WPSClient()
 #    ["5","http://services.iguess.tudor.lu/pywps/sampleData/testLines4326.gml"],
 #    ["buffer"])
 
-# Test with a WFS resource
+## Test with a WFS resource
 iniCli.init(
     # Process Server address
     "http://services.iguess.tudor.lu/cgi-bin/pywps.cgi?", 
@@ -37,6 +37,35 @@ iniCli.init(
     ["5","http://services.iguess.tudor.lu/cgi-bin/mapserv?map=/var/www/MapFiles/Europe4326.map&amp;SERVICE=WFS&amp;VERSION=1.1.0&amp;REQUEST=getfeature&amp;TYPENAME=testLines4326&amp;srsName=EPSG:900913&amp;MAXFEATURES=10"],
     # Output names
     ["buffer"])
+
+# Test with ultimate question
+#iniCli.init(
+#    # Process Server address
+#    "http://services.iguess.tudor.lu/cgi-bin/pywps.cgi?", 
+#    # Process name
+#    "ultimatequestionprocess", 
+#    # Input names
+#    [], 
+#    # Input values - '&' character must be passed as '&amp;'
+#    [],
+#    # Output names
+#    ["answer"])
+
+# Test with solar cadastre segmentation
+#iniCli.init(
+#    # Process Server address
+#    "http://services.iguess.tudor.lu/cgi-bin/pywps.cgi?", 
+#    # Process name
+#    "solar_cadastre_segment", 
+#    # Input names
+#    ["dsm","roof_training_area","building_footprints","roof_training_area_col"], 
+#    # Input values - '&' character must be passed as '&amp;'
+#    ["http://services.iguess.tudor.lu/cgi-bin/mapserv?map=/var/www/MapFiles/RO_localOWS_test.map&amp;SERVICE=WCS&amp;VERSION=1.1.0&amp;REQUEST=GetCoverage&amp;IDENTIFIER=RO_dsm&amp;FORMAT=GTiff&amp;BOUNDINGBOX=92213,436671.5,92348,436795,urn:ogc:def:crs:EPSG::28992",
+#     "http://services.iguess.tudor.lu/cgi-bin/mapserv?map=/var/www/MapFiles/RO_localOWS_test.map&amp;SERVICE=WFS&amp;VERSION=1.1.0&amp;REQUEST=getfeature&amp;TYPENAME=RO_training_areas_mini&amp;srsName=EPSG:28992",
+#     #"http://services.iguess.tudor.lu/cgi-bin/mapserv?map=/var/www/MapFiles/RO_localOWS_test.map&amp;SERVICE=WFS&amp;VERSION=1.1.0&amp;REQUEST=getfeature&amp;TYPENAME=RO_building_footprints_mini&amp;srsName=EPSG:28992",
+#     "type"],
+#    # Output names
+#    ["optimum_aspect", "optimum_slope", "ro_roof_useful_intsect_gml"])
 
 url = iniCli.sendRequest()
 
@@ -59,7 +88,8 @@ else:
     # Maybe it should be a parameter to the constructor?
     statCli.epsg = "28992"
     
-    statCli.generateMapFile()
+    path = statCli.generateMapFile()
+    print "Wrote map file to disk:\n" + path
     
     
     
